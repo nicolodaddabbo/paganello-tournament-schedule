473f0376fc6def8111ddd5a03e424c5369ee6383
Nicolò D'Addabbo <nicolodaddabbo@gmail.com>
2024-03-25 21:31:12 +0100

    Updated pool&brackets link

--- components/Navbar.js
+++ components/Navbar.js
@@ -20,7 +20,7 @@ const Navbar = () => {
                 <h1 className='header'>Schedule Paganello 2024</h1>
             </div>
             <div className='pools-button-container'>
-                <button className='pool-button' onClick={()=>window.open('https://docs.google.com/spreadsheets/d/e/2PACX-1vQ9DDndss0yEehkCrZeecEnU4EPbfSsRl-qCgWYEYxDTKx5wCCuyF94YxTcwy8hlA14wMrDNha33ryV/pubhtml?gid=85686787', '_blank')}>Standings and Pools</button>
+                <button className='pool-button' onClick={()=>window.open('https://docs.google.com/spreadsheets/d/e/2PACX-1vRCZOg_D2OD5cwGJ8or9BIPxseAy-1yycpcKu1rRb6YBLQbC65Jz6iK29_y_kz6rLNfLl7I282F4hv6/pubhtml', '_blank')}>Standings and Pools</button>
             </div>
         </div>
     )
