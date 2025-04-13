e5152266d09a1cf44d5b6116c15c08133baa5aad
parsa <hi@parsam.io>
2025-04-13 23:30:02 +0200

    navbar more mobile friendly

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,27 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import logo from "../pages/assets/Paganello_Logo_White.svg";
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <div className="navbar">
-      <div className="title">
-        <div className="link-to-main">
-          <a href="https://www.paganello.com/">
-            <Image src={logo} alt="Paganello Logo" width={100} height={100} />
+    <nav className="navbar">
+      <div className="navbar-container">
+        <div className="navbar-logo">
+          <a href="https://www.paganello.com/" className="logo-link">
+            <Image src={logo} alt="Paganello Logo" width={80} height={80} />
+          </a>
+          <h1 className="navbar-title">Schedule 2025</h1>
+        </div>
+
+        <div className="hamburger" onClick={toggleMenu}>
+          <span
+            className={`hamburger-line ${isMenuOpen ? "active" : ""}`}
+          ></span>
+          <span
+            className={`hamburger-line ${isMenuOpen ? "active" : ""}`}
+          ></span>
+          <span
+            className={`hamburger-line ${isMenuOpen ? "active" : ""}`}
+          ></span>
+        </div>
+
+        <div className={`navbar-links ${isMenuOpen ? "active" : ""}`}>
+          <a
+            className="pool-button"
+            href="https://docs.google.com/spreadsheets/d/e/2PACX-1vRCZOg_D2OD5cwGJ8or9BIPxseAy-1yycpcKu1rRb6YBLQbC65Jz6iK29_y_kz6rLNfLl7I282F4hv6/pubhtml"
+          >
+            Standings and Pools
           </a>
         </div>
-        <h1 className="header">Schedule 2025</h1>
       </div>
-      <div className="pools-button-container">
-        <a
-          className="pool-button"
-          href="https://docs.google.com/spreadsheets/d/e/2PACX-1vRCZOg_D2OD5cwGJ8or9BIPxseAy-1yycpcKu1rRb6YBLQbC65Jz6iK29_y_kz6rLNfLl7I282F4hv6/pubhtml"
-        >
-          Standings and Pools
-        </a>
-      </div>
-    </div>
+    </nav>
   );
 };
 
